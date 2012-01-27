c36fd21b95e14eba35b5a7ae8c1bd2e2ef9bfab8
Patrick Ewing <patrick@twitter.com>
2012-01-26 23:06:02 -0800

    Read conversation data from file passed via CLI

--- convo.js
+++ convo.js
@@ -1,106 +1,18 @@
-var prompt = require('prompt')
-var _ = require("underscore")
 require("colors")
+var prompt = require('prompt')
+prompt.message = ">>".green
+prompt.start()
 
-conv =
-{
-    "children": [
-        {
-            "children": [
-                {
-                    "children": [
-                        {
-                            "children": [
-                                {
-                                    "children": [
-                                        {
-                                            "children": [
-                                                {
-                                                    "children": [],
-                                                    "name": "Let's be friends"
-                                                }
-                                            ],
-                                            "name": "Yes"
-                                        },
-                                        {
-                                            "children": [
-                                                {
-                                                    "children": [],
-                                                    "name": "Let's be friends"
-                                                }
-                                            ],
-                                            "name": "No"
-                                        },
-                                        {
-                                            "children": [
-                                                {
-                                                    "children": [],
-                                                    "name": "You're dead to me."
-                                                }
-                                            ],
-                                            "name": "sorta\n"
-                                        }
-                                    ],
-                                    "name": "do you like the Smiths?\n"
-                                }
-                            ],
-                            "name": "I'm from Sacramento"
-                        }
-                    ],
-                    "name": "Then why do you say hella all the time?"
-                }
-            ],
-            "name": "I'm from NYC"
-        },
-        {
-            "children": [
-                {
-                    "children": [
-                        {
-                            "children": [
-                                {
-                                    "children": [],
-                                    "name": "Let's be friends"
-                                }
-                            ],
-                            "name": "Yes"
-                        },
-                        {
-                            "children": [
-                                {
-                                    "children": [],
-                                    "name": "Let's be friends"
-                                }
-                            ],
-                            "name": "No"
-                        },
-                        {
-                            "children": [
-                                {
-                                    "children": [],
-                                    "name": "You're dead to me."
-                                }
-                            ],
-                            "name": "sorta\n"
-                        }
-                    ],
-                    "name": "do you like the Smiths?\n"
-                }
-            ],
-            "name": "I'm from Sacramento"
-        }
-    ],
-    "name": "Where do you come from?"
-}
-prompt.message = ">>".green;
-prompt.start();
+var _ = require("underscore")
+var fs = require('fs')
+var arguments = process.argv.splice(2)
 
-askQuestions(conv);
+var conv = JSON.parse(fs.readFileSync(arguments[0], 'utf8'))
 
 function askQuestions(n) {
   responses = n.children.map(function(i) {
     return i.name
-  });
+  })
 
   var question = {
     name: n.name,
@@ -108,34 +20,36 @@ function askQuestions(n) {
   }
 
   prompt.get([question], function (err, result) {
-    if (err) { return onErr(err); }
-    var selection = result[n.name];
+    if (err) { return onErr(err) }
+    var selection = result[n.name]
 
     var choice = n.children.filter(function(i) {
       return i.name.trim() == selection
     })
 
     if (choice.length == 1) {
-      var whatsNext = choice[0].children[0];
+      var whatsNext = choice[0].children[0]
 
       if (whatsNext.children.length) {
         askQuestions(whatsNext)
       } else {
-        convoOver(whatsNext.name);
+        convoOver(whatsNext.name)
       }
     } else {
       throw "Not a valid choice"
     }
-
-  });
+  })
 }
 
 function convoOver(message) {
   console.log(message.magenta)
-  return 1;
+  return 1
 }
 
 function onErr(err) {
-  console.log("ERROR!!".red, err);
-  return 1;
+  console.log("ERROR!!".red, err)
+  return 0
 }
+
+// Start it off
+askQuestions(conv)
