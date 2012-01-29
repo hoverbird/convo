c56472cccd500b79b7512049272c1a65802d8580
Patrick Ewing <patrick@twitter.com>
2012-01-28 20:15:45 -0800

    More modular, better tests

--- test/convo-test.js
+++ test/convo-test.js
@@ -4,6 +4,7 @@ var path = require('path'),
     vows = require('vows'),
     assert = require('assert'),
     gently = new (require('gently')),
+    winston = require('winston'),
     convo = require('convo')
 
 vows.describe('Convo').addBatch({
@@ -18,14 +19,44 @@ vows.describe('Convo').addBatch({
       gently.expect(convo.prompt, 'get', function(msg) {
         assert.isArray(msg)
         var question = msg[0]
-        assert.isObject(question)
 
+        assert.isObject(question)
         assert.match(question.name, /What is yr business, stranger?/)
         assert.match(question.message, /What is yr business, stranger?/)
         assert.match(question.message, /Whatsit to you, copper?/)
       });
 
       convo.askQuestions(convoData[0])
+    }
+  },
+
+  'interacting with the prompt': {
+    topic: function () {
+      winston.info('These prompt tests are interactive');
+      winston.info('Not following instructions will result in test failure');
+
+      convo.askQuestions({
+        "name": "What is yr business, stranger?",
+        "children": [{"name": "Whatsit to you, copper?", "children": [{"name": "Yr under arrest."}]}]
+      })
+
+      return null;
+    },
+
+    ".askQuestions": {
+      "when passed a node with no children": {
+        topic: function () {
+          winston.info('When prompted, enter: 1 [enter]');
+          gently.expect(convo, 'convoOver', function(msg) {
+             assert.equal(msg.name, "Yr under arrest.")
+           })
+          convo.prompt.getInput('1', this.callback);
+        },
+        "gets user input and then ends the convo ": function (err, result) {
+          assert.isNull(err);
+          assert.equal(result, '1');
+        }
+      }
     }
   }
 }).exportTo(module);
